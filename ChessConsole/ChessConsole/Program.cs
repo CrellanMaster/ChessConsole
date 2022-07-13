ce94e48d224e20aa0babe3de4e0113f9b69a0477
CrellanMaster <devsandrounity@gmail.com>
2022-07-12 22:53:29 -0300

    Created Position Class

---
+++ ChessConsole/ChessConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using board;
+
+namespace ChessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
+        }
+    }
+}
