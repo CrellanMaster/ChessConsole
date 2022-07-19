e6efa4f3f8746e91bc7ae26de900408198986aaa
CrellanMaster <devsandrounity@gmail.com>
2022-07-19 00:01:13 -0300

    Creating the chess position class

--- ChessConsole/ChessConsole/Program.cs
+++ ChessConsole/ChessConsole/Program.cs
@@ -8,21 +8,9 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board tab = new Board(8, 8);
-
-                tab.PlacePart(new Tower(Color.Preta, tab), new Position(0, 0));
-                tab.PlacePart(new Tower(Color.Preta, tab), new Position(1, 9));
-                tab.PlacePart(new King(Color.Preta, tab), new Position(0, 0));
-
-                Screen.ShowBoard(tab);
-                Console.WriteLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            ChessPosition pos = new ChessPosition('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosition());
             Console.ReadLine();
         }
     }
